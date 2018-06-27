dd328ec510741897bb7efb657d287943c2800a76
Nilesh Magan <nilesh.magan@gmail.com>
2018-06-27 16:48:11 +1200

    Eclipse code is good, adding more memory

--- accumulate/accumulate.sdk/Test_accumulate/Debug/src/helloworld.d
+++ accumulate/accumulate.sdk/Test_accumulate/Debug/src/helloworld.d
@@ -1,18 +1,48 @@
 src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
  ../src/platform_config.h \
- ../../Test_accumulate_bsp/microblaze_0/include/xil_printf.h \
- ../../Test_accumulate_bsp/microblaze_0/include/xil_types.h \
  ../../Test_accumulate_bsp/microblaze_0/include/xparameters.h \
- ../../Test_accumulate_bsp/microblaze_0/include/bspconfig.h
+ ../../Test_accumulate_bsp/microblaze_0/include/xintc.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xil_types.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xil_assert.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xparameters.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xstatus.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xintc_l.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xil_io.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xil_printf.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/bspconfig.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/mb_interface.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xil_exception.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/XHls_macc.h \
+ ../../Test_accumulate_bsp/microblaze_0/include/xhls_macc_hw.h
 
 ../src/platform.h:
 
 ../src/platform_config.h:
 
-../../Test_accumulate_bsp/microblaze_0/include/xil_printf.h:
+../../Test_accumulate_bsp/microblaze_0/include/xparameters.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xintc.h:
 
 ../../Test_accumulate_bsp/microblaze_0/include/xil_types.h:
 
+../../Test_accumulate_bsp/microblaze_0/include/xil_assert.h:
+
 ../../Test_accumulate_bsp/microblaze_0/include/xparameters.h:
 
+../../Test_accumulate_bsp/microblaze_0/include/xstatus.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xintc_l.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xil_io.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xil_printf.h:
+
 ../../Test_accumulate_bsp/microblaze_0/include/bspconfig.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/mb_interface.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xil_exception.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/XHls_macc.h:
+
+../../Test_accumulate_bsp/microblaze_0/include/xhls_macc_hw.h:
